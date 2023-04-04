08c6738b7e102aaedc0445709385a00efc31923d
peiminmin <peiminmin@yeah.net>
2023-04-04 19:21:57 +0800

    练习1

--- r/trait-model/src/lib.rs
+++ r/trait-model/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug, Display};
+
 // 定义云主机开机操作助手trait: 泛型参数版
 trait CloudOperator {
     fn create_instance(&self) {
@@ -35,6 +37,55 @@ impl PublicCloudOperator for TencentOperator {
     }
 }
 
+fn create_public_instance<T>(_operator: &T)
+where
+    T: PublicCloudOperator,
+{
+}
+
+fn create_private_instance<T: PrivateCloudOperator>(_operator: &T) {}
+
+//trait 增强泛型的约束实现
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn new(x: T, y: T) -> Self {
+        Point { x, y }
+    }
+}
+
+impl<T: PartialOrd + Display> Point<T> {
+    fn compare_display(&self) {
+        if self.x > self.y {
+            println!("the largest number is x = {}", self.x)
+        } else {
+            println!("the largest number is y = {}", self.y)
+        }
+    }
+}
+
+//trait 关联类型
+trait StreamingIterator {
+    type Item: Debug + Display; //关联类型添加约束
+}
+
+struct A;
+
+impl StreamingIterator for A {
+    type Item = String;
+}
+
+struct B<T>
+where
+    T: StreamingIterator<Item = String>,
+{
+    a: T,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +97,8 @@ mod tests {
 
         let tencent_operator = TencentOperator {};
         tencent_operator.create_instance();
+
+        let a = A;
+        let b = B { a };
     }
 }
