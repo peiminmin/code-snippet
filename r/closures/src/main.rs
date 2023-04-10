f099ea2ef0dbeff1beea1467d4a89123a576cad4
裴旻敏 <peiminmin@yeah.net>
2023-04-10 09:31:33 +0800

    变更

--- r/closures/src/main.rs
+++ r/closures/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::thread;
 
 fn main() {
@@ -15,10 +16,12 @@ fn main() {
     // only_borrow(); 如果写在这里，如果写在这里，发生了可变借用于不可变借用作用域存在交叉
 
     //线程运行
-    thread_print();
+    //thread_print();
 
     //线程启动如果不join到主线程，会导致子线程成为孤儿线程
-    thread_print2();
+    //thread_print2();
+
+    thread_caculate();
 }
 
 fn thread_print() {
@@ -56,4 +59,34 @@ fn thread_print3() {
     let handler = thread::spawn(move || println!("{:?}", list)); //多线程不能引用外部的资源
 
     handler.join().unwrap();
+}
+
+fn thread_caculate() -> Result<(), String> {
+    let numbers = vec![1, 2, 3, 4, 5, 6];
+
+    let (avg, mode) = thread::scope(|scope| {
+        let avg_thread = scope.spawn(|| numbers.iter().sum::<i32>() as f32 / numbers.len() as f32);
+
+        let mod_thread = scope.spawn(|| {
+            let mut counter = HashMap::new();
+            for item in numbers.iter() {
+                *counter.entry(item).or_insert(0) += 1;
+            }
+
+            return counter
+                .into_iter()
+                .max_by_key(|&(_, value)| value) //&(_, value) ??
+                .map(|(key, _)| key);
+        });
+
+        (avg_thread.join(), mod_thread.join())
+    });
+
+    let (avg, mode) = match (avg, mode) {
+        (Ok(avg), Ok(Some(mode))) => Ok((avg, mode)),
+        _ => Err("failed".to_string()),
+    }?;
+
+    println!("the median is {avg} and the mode is {mode}");
+    Ok(())
 }
